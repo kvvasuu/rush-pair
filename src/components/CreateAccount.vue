e6d4a71f879e2097df3cff35206f5ce502b00f3d
Kwasu <lukaszkwas96@gmail.com>
2024-10-09 15:48:37 +0200

    .

--- src/components/CreateAccount.vue
+++ src/components/CreateAccount.vue
@@ -1,14 +1,44 @@
 <template>
   <BasicModal :prevent-close="preventModalClose" @close="closeModal">
-    <div class="w-full h-full flex flex-col items-center">
+    <div
+      class="w-full h-full flex flex-col items-center pb-6"
+      v-if="!registerComplete"
+    >
       <header class="flex flex-col items-center mb-6">
-        <img src="../../public/logo_sygnet.png" alt="Rush Pair" width="52px" />
+        <img src="/logo_sygnet.png" alt="Rush Pair" width="52px" />
         <h1 class="text-3xl font-bold text-center mt-6">Create account</h1>
-        <p class="min-h-6">
+        <p class="min-h-6 mt-2 text-center">
           <span v-if="generalError">{{ generalError }}</span>
         </p>
       </header>
-      <form class="w-full flex flex-col items-center justify-center">
+      <div
+        role="status"
+        class="flex items-center justify-center absolute h-full top-0"
+        v-if="isLoading"
+      >
+        <svg
+          aria-hidden="true"
+          class="w-8 h-8 text-gray-200 animate-spin fill-gray-500"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+        <span class="sr-only">Loading...</span>
+      </div>
+      <form
+        class="w-full h-full flex flex-col items-center justify-center"
+        @submit.prevent="register"
+        v-else
+      >
         <div
           class="mb-3 w-full flex flex-col items-center relative"
           :class="{ 'mb-0': showEmailError }"
@@ -29,7 +59,7 @@
           ></i>
           <span
             v-if="showEmailError"
-            class="text-red-500 font-semibold text-xs w-full text-left pl-4 py-1"
+            class="text-red-500 font-semibold text-xs w-full text-left pl-4 pt-1"
             >Please provide correct email.</span
           >
         </div>
@@ -42,7 +72,12 @@
             placeholder="Password"
             type="password"
             v-model="password"
-            @blur="validatePassword"
+            @blur="
+              () => {
+                validatePasswordConfirm;
+                validatePasswordConfirm;
+              }
+            "
             @click="showPasswordError = false"
             class="w-full p-4 pl-12 rounded-xl border-2"
             :class="{ 'border-red-500': showPasswordError }"
@@ -54,7 +89,7 @@
 
           <span
             v-if="showPasswordError"
-            class="text-red-500 font-semibold text-xs w-full text-left pl-4 py-1"
+            class="text-red-500 font-semibold text-xs w-full text-left pl-4 pt-1"
             >Password must be at least 6 characters long.</span
           >
         </div>
@@ -67,7 +102,12 @@
             placeholder="Confirm password"
             type="password"
             v-model="passwordConfirm"
-            @blur="validatePasswordConfirm"
+            @blur="
+              () => {
+                validatePasswordConfirm;
+                validatePasswordConfirm;
+              }
+            "
             @click="showPasswordConfirmError = false"
             class="w-full p-4 pl-12 rounded-xl border-2"
             :class="{ 'border-red-500': showPasswordConfirmError }"
@@ -79,13 +119,37 @@
 
           <span
             v-if="showPasswordConfirmError"
-            class="text-red-500 font-semibold text-xs w-full text-left pl-4 py-1"
+            class="text-red-500 font-semibold text-xs w-full text-left pl-4 pt-1"
             >Passwords are not the same.</span
           >
         </div>
+        <div class="flex min-h-10 w-full justify-center mt-2">
+          <div class="flex items-center h-5">
+            <input
+              id="terms"
+              name="terms"
+              type="checkbox"
+              value="terms"
+              v-model="checkbox"
+              @change="validateTerms"
+              class="w-4 h-4"
+            />
+          </div>
+          <div class="ms-2">
+            <label for="terms" class="text-gray-900"
+              >I agree to the terms and conditions</label
+            >
+            <p
+              class="text-xs font-semibold text-red-500"
+              v-if="showCheckboxError"
+            >
+              You must agree with terms and conditions
+            </p>
+          </div>
+        </div>
         <button
-          class="px-8 py-3 w-full mt-8 sm:w-auto font-bold text-lg bg-main-gradient hover:bg-main-gradient-dark text-slate-50 rounded-full transition-all drop-shadow-sm"
-          @click="register"
+          class="px-8 py-3 w-full sm:w-auto font-bold text-lg bg-main-gradient hover:bg-main-gradient-dark text-slate-50 rounded-full transition-all drop-shadow-sm mt-auto"
+          type="submit"
         >
           Create account
         </button>
@@ -126,7 +190,7 @@ const showCheckboxError = ref(false);
 const isLoading = ref(false);
 const registerComplete = ref(false);
 
-const generalError = ref("Something went wrong. Try again later.");
+const generalError = ref("");
 
 const preventModalClose = computed(() => {
   return !!emailCorrect.value || !!password.value || !!passwordConfirm.value;
@@ -180,13 +244,13 @@ const register = async () => {
   validatePasswordConfirm();
   validateTerms();
   generalError.value = "";
-  isLoading.value = true;
   if (
     emailCorrect.value &&
     passwordCorrect.value &&
     passwordConfirmCorrect.value &&
     checkbox.value
   ) {
+    isLoading.value = true;
     await axios
       .post("http://localhost:3000/auth/register", {
         name: "Łukasz Kwas",
@@ -207,6 +271,10 @@ const register = async () => {
         console.log(error);
       })
       .finally(() => {
+        email.value = "";
+        password.value = "";
+        passwordConfirm.value = "";
+        checkbox.value = false;
         isLoading.value = false;
       });
   }
