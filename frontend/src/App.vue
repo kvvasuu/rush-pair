970c1b2ccc14daef2970697a96ce5be06fb80cbd
Kwasu <lukaszkwas96@gmail.com>
2024-10-24 14:14:38 +0200

    DeleteAccountOverlay

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <RouterView></RouterView>
+  <RouterView v-slot="{ Component }">
+    <Transition name="fade" mode="out-in">
+      <Component :is="Component" />
+    </Transition>
+  </RouterView>
 </template>
 
 <script setup lang="ts">
