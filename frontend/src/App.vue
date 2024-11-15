c8111fdfab9ded9ab52ff6291eb584f7a725614f
Kwasu <lukaszkwas96@gmail.com>
2024-11-15 10:19:13 +0100

    HorizontalScreenWarning added. Portrait mode only.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <HorizontalScreenWarning
+    v-if="displayHorizontalScreenWarning"
+  ></HorizontalScreenWarning>
   <RouterView v-slot="{ Component }">
     <Transition name="fade" mode="out-in">
       <Component :is="Component" />
@@ -7,10 +10,21 @@
 </template>
 
 <script setup lang="ts">
-import { onBeforeMount } from "vue";
+import { onBeforeMount, ref } from "vue";
 import { useUserStore } from "./stores/userStore";
+import HorizontalScreenWarning from "./components/HorizontalScreenWarning.vue";
 
 const userStore = useUserStore();
+
+const displayHorizontalScreenWarning = ref(false);
+const checkOrientation = () => {
+  displayHorizontalScreenWarning.value =
+    window.matchMedia("(orientation: landscape)").matches &&
+    window.innerHeight <= 500;
+};
+
+window.addEventListener("resize", checkOrientation);
+window.addEventListener("load", checkOrientation);
 
 onBeforeMount(async () => {
   await userStore.login();
---
+++ frontend/src/components/HorizontalScreenWarning.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="absolute top-0 left-0 w-dvw h-dvh flex flex-col gap-6 items-center justify-center bg-neutral-900/90 backdrop-blur-sm z-50 text-center text-neutral-200 pointer-events-none"
+  >
+    <i class="fa-solid fa-rotate text-4xl rotate"></i>
+    <div>
+      <p class="text-lg font-semibold">
+        Rotate your device to use the application.
+      </p>
+      <p class="text-xs">
+        This application is designed to work in portrait mode only.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.rotate {
+  animation: rotate 3s infinite;
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(-45deg);
+  }
+  75% {
+    transform: rotate(90deg);
+  }
+  100% {
+    transform: rotate(-45deg);
+  }
+}
+</style>
