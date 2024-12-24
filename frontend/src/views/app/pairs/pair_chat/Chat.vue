60e19c204b51025f4655dcc38e7b4c3c0b8ecc0f
Kwasu <lukaszkwas96@gmail.com>
2024-12-24 15:27:10 +0100

    ...

--- frontend/src/views/app/pairs/pair_chat/Chat.vue
+++ frontend/src/views/app/pairs/pair_chat/Chat.vue
@@ -3,7 +3,7 @@
     class="flex-1 h-full w-full flex flex-col lg:flex-col justify-end relative"
   >
     <div
-      class="absolute bottom-16 w-full h-[calc(100%-4rem)] overflow-y-auto overflow-x-hidden flex flex-col-reverse gap-1"
+      class="w-full h-full relative overflow-y-auto overflow-x-hidden flex flex-col-reverse gap-1"
     >
       <div
         class="w-full h-full flex items-center justify-center"
@@ -19,49 +19,51 @@
       ></ChatMessagesList>
     </div>
 
-    <div class="w-full h-16 absolute">
-      <input
-        name="message"
-        id="message"
-        v-model="message"
-        type="text"
-        class="w-full h-full m-0 outline-none p-2 pl-4 text-2xl bg-neutral-50 hover:bg-neutral-100/90 dark:bg-neutral-800/80 dark:hover:bg-neutral-800/50 text-neutral-600 placeholder-neutral-300 dark:text-neutral-400 dark:placeholder-neutral-700 transition-all shadow z-30"
-        :class="[isTouchDevice ? 'pr-14' : 'pr-32']"
-        placeholder="Type a message"
-        autocomplete="off"
-        spellcheck="false"
-        @input="onInput"
-        @keydown.enter="sendMessage"
-        ref="messageInputRef"
-      /><button
-        class="w-16 h-full absolute right-0 text-rose-500 hover:text-rose-600 transition-all duration-300 text-2xl hover:text-3xl"
-        :title="message ? `Send message` : `Send heart`"
-        @click="sendMessage"
+    <div
+      name="message"
+      id="message"
+      contenteditable="true"
+      class="w-full h-auto overflow-y-auto overflow-x-hidden max-h-32 outline-none p-4 pl-4 text-2xl bg-neutral-50 hover:bg-neutral-100/90 dark:bg-neutral-800/80 dark:hover:bg-neutral-800/50 text-neutral-600 placeholder-neutral-300 dark:text-neutral-400 dark:placeholder-neutral-700 transition-all z-20"
+      :class="[isTouchDevice ? 'pr-14' : 'pr-32']"
+      placeholder="Type a message..."
+      autocomplete="off"
+      spellcheck="false"
+      @input="onInput"
+      @keydown.enter="sendMessage"
+      @paste="handlePaste"
+      ref="messageInputRef"
+    ></div>
+    <button
+      class="w-16 h-16 absolute bottom-0 right-0 text-rose-500 hover:text-rose-600 transition-all duration-300 text-2xl hover:text-3xl z-20"
+      :title="message ? `Send message` : `Send heart`"
+      @click="sendMessage"
+    >
+      <Transition name="pop-up-fast" mode="out-in">
+        <i class="fa-solid fa-paper-plane" v-if="message"></i>
+        <i class="fa-solid fa-heart" v-else></i>
+      </Transition>
+    </button>
+    <div
+      class="w-16 h-16 bottom-0 absolute right-16 z-20"
+      v-if="!isTouchDevice"
+    >
+      <button
+        class="w-full h-full absolute text-neutral-600 text-2xl group"
+        title="Select emoji"
+        @click.stop="toggleEmojiSelector"
       >
-        <Transition name="pop-up-fast" mode="out-in">
-          <i class="fa-solid fa-paper-plane" v-if="message"></i>
-          <i class="fa-solid fa-heart" v-else></i>
-        </Transition>
-      </button>
-      <div class="w-16 h-full top-0 absolute right-16" v-if="!isTouchDevice">
-        <button
-          class="w-full h-full absolute text-neutral-600 text-2xl group"
-          title="Select emoji"
-          @click.stop="toggleEmojiSelector"
-        >
-          <i
-            class="fa-regular fa-face-smile group-hover:bg-neutral-200 dark:group-hover:bg-neutral-800 p-2 rounded-full transition-all duration-300"
-            :class="{
-              'bg-neutral-200 dark:bg-neutral-800': isEmojiSelectorVisible,
-            }"
-          ></i></button
-        ><EmojiPicker
-          class="absolute h-[19rem] -top-[17.5rem] w-[19.5rem] -left-72 rounded-br-none shadow-lg"
-          @select-emoji="selectEmoji"
-          @close="isEmojiSelectorVisible = false"
-          v-if="isEmojiSelectorVisible"
-        ></EmojiPicker>
-      </div>
+        <i
+          class="fa-regular fa-face-smile group-hover:bg-neutral-200 dark:group-hover:bg-neutral-800 p-2 rounded-full transition-all duration-300"
+          :class="{
+            'bg-neutral-200 dark:bg-neutral-800': isEmojiSelectorVisible,
+          }"
+        ></i></button
+      ><EmojiPicker
+        class="absolute h-[19rem] -top-[17.5rem] w-[19.5rem] -left-72 rounded-br-none shadow-lg"
+        @select-emoji="selectEmoji"
+        @close="isEmojiSelectorVisible = false"
+        v-if="isEmojiSelectorVisible"
+      ></EmojiPicker>
     </div>
   </div>
 </template>
@@ -92,7 +94,11 @@ const toggleEmojiSelector = () => {
 
 const selectEmoji = (emoji: string) => {
   message.value += emoji;
-  messageInputRef.value?.focus();
+
+  if (messageInputRef.value) {
+    messageInputRef.value.innerText += emoji;
+    messageInputRef.value?.focus();
+  }
 };
 
 const sendMessage = async () => {
@@ -116,7 +122,10 @@ const sendMessage = async () => {
 const typingTimeout = ref<ReturnType<typeof setTimeout> | null>(null);
 const isTyping = ref(false);
 
-const onInput = () => {
+const onInput = (event: Event): void => {
+  const target = event.target as HTMLElement;
+  if (target) message.value = target.innerText.trim();
+
   if (!isTyping.value) {
     chatStore.startTyping();
     isTyping.value = true;
@@ -130,6 +139,16 @@ const onInput = () => {
     chatStore.stopTyping();
     isTyping.value = false;
   }, 2000);
+};
+
+const handlePaste = (event: ClipboardEvent): void => {
+  event.preventDefault();
+  let plainText = "";
+  if (event.clipboardData) {
+    plainText = event.clipboardData.getData("text/plain");
+  }
+
+  document.execCommand("insertText", false, plainText);
 };
 
 onMounted(async () => {
