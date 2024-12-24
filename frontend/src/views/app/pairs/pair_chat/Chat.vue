a3e84a33ceda643bad050ded66d1667a2543e1eb
Kwasu <lukaszkwas96@gmail.com>
2024-12-24 16:17:52 +0100

    ...

--- frontend/src/views/app/pairs/pair_chat/Chat.vue
+++ frontend/src/views/app/pairs/pair_chat/Chat.vue
@@ -20,50 +20,50 @@
     </div>
 
     <div
-      name="message"
-      id="message"
-      contenteditable="true"
-      class="w-full h-auto overflow-y-auto overflow-x-hidden max-h-32 outline-none p-4 pl-4 text-2xl bg-neutral-50 hover:bg-neutral-100/90 dark:bg-neutral-800/80 dark:hover:bg-neutral-800/50 text-neutral-600 placeholder-neutral-300 dark:text-neutral-400 dark:placeholder-neutral-700 transition-all z-20"
-      :class="[isTouchDevice ? 'pr-14' : 'pr-32']"
-      placeholder="Type a message..."
-      autocomplete="off"
-      spellcheck="false"
-      @input="onInput"
-      @keydown.enter="sendMessage"
-      @paste="handlePaste"
-      ref="messageInputRef"
-    ></div>
-    <button
-      class="w-16 h-16 absolute bottom-0 right-0 text-rose-500 hover:text-rose-600 transition-all duration-300 text-2xl hover:text-3xl z-20"
-      :title="message ? `Send message` : `Send heart`"
-      @click="sendMessage"
+      class="flex w-full items-end break-all h-auto max-h-36 pr-2 bg-neutral-50 hover:bg-neutral-100/90 dark:bg-neutral-800/80 dark:hover:bg-neutral-800/50 text-neutral-600 placeholder-neutral-300 dark:text-neutral-400 dark:placeholder-neutral-700 transition-all z-20"
     >
-      <Transition name="pop-up-fast" mode="out-in">
-        <i class="fa-solid fa-paper-plane" v-if="message"></i>
-        <i class="fa-solid fa-heart" v-else></i>
-      </Transition>
-    </button>
-    <div
-      class="w-16 h-16 bottom-0 absolute right-16 z-20"
-      v-if="!isTouchDevice"
-    >
+      <p
+        name="message"
+        id="message"
+        contenteditable="true"
+        class="w-full break-all h-auto max-h-36 overflow-y-auto p-4 pr-0 overflow-x-hidden outline-none text-2xl"
+        placeholder="Type a message..."
+        autocomplete="off"
+        spellcheck="false"
+        @input="onInput"
+        @keydown.enter="sendMessage"
+        @paste="handlePaste"
+        ref="messageInputRef"
+      ></p>
+      <div class="w-16 h-16 z-20 relative" v-if="!isTouchDevice">
+        <button
+          class="w-full h-full text-neutral-600 text-2xl group"
+          title="Select emoji"
+          @click.stop="toggleEmojiSelector"
+        >
+          <i
+            class="fa-regular fa-face-smile group-hover:bg-neutral-200 dark:group-hover:bg-neutral-800 p-2 rounded-full transition-all duration-300"
+            :class="{
+              'bg-neutral-200 dark:bg-neutral-800': isEmojiSelectorVisible,
+            }"
+          ></i></button
+        ><EmojiPicker
+          class="absolute h-[19rem] -top-[17.5rem] w-[19.5rem] -left-72 rounded-br-none shadow-lg"
+          @select-emoji="selectEmoji"
+          @close="isEmojiSelectorVisible = false"
+          v-if="isEmojiSelectorVisible"
+        ></EmojiPicker>
+      </div>
       <button
-        class="w-full h-full absolute text-neutral-600 text-2xl group"
-        title="Select emoji"
-        @click.stop="toggleEmojiSelector"
+        class="w-16 h-16 text-rose-500 hover:text-rose-600 transition-all duration-300 text-2xl hover:text-3xl z-20"
+        :title="message ? `Send message` : `Send heart`"
+        @click="sendMessage"
       >
-        <i
-          class="fa-regular fa-face-smile group-hover:bg-neutral-200 dark:group-hover:bg-neutral-800 p-2 rounded-full transition-all duration-300"
-          :class="{
-            'bg-neutral-200 dark:bg-neutral-800': isEmojiSelectorVisible,
-          }"
-        ></i></button
-      ><EmojiPicker
-        class="absolute h-[19rem] -top-[17.5rem] w-[19.5rem] -left-72 rounded-br-none shadow-lg"
-        @select-emoji="selectEmoji"
-        @close="isEmojiSelectorVisible = false"
-        v-if="isEmojiSelectorVisible"
-      ></EmojiPicker>
+        <Transition name="pop-up-fast" mode="out-in">
+          <i class="fa-solid fa-paper-plane" v-if="message"></i>
+          <i class="fa-solid fa-heart" v-else></i>
+        </Transition>
+      </button>
     </div>
   </div>
 </template>
@@ -97,7 +97,12 @@ const selectEmoji = (emoji: string) => {
 
   if (messageInputRef.value) {
     messageInputRef.value.innerText += emoji;
-    messageInputRef.value?.focus();
+    const windowSelection = window.getSelection();
+    if (windowSelection) {
+      messageInputRef.value?.focus();
+      windowSelection.selectAllChildren(messageInputRef.value);
+      windowSelection.collapseToEnd();
+    }
   }
 };
 
