edd3ab2f391b0e36fb38c8ebd6b55278693c3797
kvvasu <lukaszkwas96@gmail.com>
2025-01-18 12:40:02 +0100

    .

--- frontend/src/views/welcome_screen/WelcomeScreen.vue
+++ frontend/src/views/welcome_screen/WelcomeScreen.vue
@@ -62,7 +62,7 @@
     <footer
       class="absolute bottom-0 left-0 w-full select-none text-[0.5rem] md:text-xs text-neutral-900/50 font-semibold text-center md:text-right px-2"
     >
-      <p>© 2024 RushPair. All Rights Reserved.</p>
+      <p>{{ t("welcomeScreen.allRightsReserved") }}</p>
     </footer>
   </main>
 </template>
