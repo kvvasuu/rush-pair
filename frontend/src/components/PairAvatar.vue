da2cba1e6bc68aee4d462871a32da098dc3f6d14
Kwasu <lukaszkwas96@gmail.com>
2025-02-10 14:58:38 +0100

    Added images skeleton loaders.

--- frontend/src/components/PairAvatar.vue
+++ frontend/src/components/PairAvatar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="select-none rounded-full pair">
+  <div class="select-none rounded-full" :class="{ pair: isLoaded }">
     <div class="relative">
       <img
         :src="avatarSrc"
@@ -9,10 +9,17 @@
         :class="{ 'rounded-none': square }"
         draggable="false"
         @error="setDefaultAvatar"
+        @load="onLoad"
+        v-show="isLoaded"
       />
       <div
         class="absolute rounded-full border-2 border-slate-200 dark:border-neutral-900 bg-lime-500 w-[30%] h-[30%] bottom-0 right-0"
         v-if="isActive && !square"
+      ></div>
+      <div
+        class="w-full h-full aspect-square bg-stone-100 dark:bg-neutral-800/60 animate-pulse"
+        :class="{ 'rounded-full': !square }"
+        v-if="!isLoaded"
       ></div>
     </div>
   </div>
@@ -20,7 +27,14 @@
 
 <script setup lang="ts">
 import { useAvatar } from "../composables/avatar.ts";
+import { ref } from "vue";
 const props = defineProps(["pair", "square", "isActive"]);
+
+const isLoaded = ref(false);
+
+const onLoad = () => {
+  isLoaded.value = true;
+};
 
 const { setDefaultAvatar, avatarSrc } = useAvatar(
   props.pair?.imageUrl,
--- frontend/src/components/UserAvatar.vue
+++ frontend/src/components/UserAvatar.vue
@@ -9,13 +9,26 @@
       draggable="false"
       @error="setDefaultAvatar"
       class="object-cover h-full"
+      @load="onLoad"
+      v-show="isLoaded"
     />
+    <div
+      class="w-full h-full aspect-square rounded-full bg-stone-100 dark:bg-neutral-800/60 animate-pulse"
+      v-if="!isLoaded"
+    ></div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useUserStore } from "../stores/userStore";
 import { useAvatar } from "../composables/avatar";
+import { ref } from "vue";
+
+const isLoaded = ref(false);
+
+const onLoad = () => {
+  isLoaded.value = true;
+};
 
 const userStore = useUserStore();
 
